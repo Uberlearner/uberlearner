8f7ea695175551963c4a6c95d03b81114c66d280
Abhin Chhabra <chhabra.abhin@gmail.com>
2012-12-01 18:32:27 -0700

    Made the photo list javascript more object oriented.

--- uberlearner/static/uberlearner/js/filestorage/uberphoto/list/viewmodel.js
+++ uberlearner/static/uberlearner/js/filestorage/uberphoto/list/viewmodel.js
@@ -1,113 +1,92 @@
-define(['ko', 'uberlearner/js/utils/dom'], function(ko, domUtils) {
-    var pageName = function(title, action, isActive, isEnabled) {
-        return {
-            title: title,
-            action: action,
-            isActive: ko.observable(isActive),
-            isEnabled: isEnabled
-        }
-    };
-
+define(['ko', 'uberlearner/js/utils/dom', 'uberlearner/js/filestorage/uberphoto/list/models'], function(ko, domUtils, Models) {
     var viewModelFactory = function() {
         // PRIVATE SECTION
-        var imageData = ko.observableArray([]); //this will be auto-populated by the url-list binding
+        /**
+         * The ELLIPSES_PAGE is a dummy page that does not actually lead to images being displayed. It is merely for
+         * the purpose of filling in gaps in the pagination buttons.
+         * @type {Models.Page}
+         */
+        var ELLIPSES_PAGE = new Models.page([], '...', function(){}, false, false);
+        var EMPTY_PAGE = new Models.page([], '', function(){}, false, false);
+        var imageUrls = ko.observableArray([]); //this will be auto-populated by the url-list binding
+        var imageData = ko.observableArray([]); //this will contain a list of page instances
         var currentImage= ko.observable();
+        var currentPage = ko.observable(EMPTY_PAGE);
         var THUMBS_PER_PAGE = 15;
-        var MAX_PAGE_TITLES = 7;
-        /**
-         * A one-indexed number corresponding to the current page being displayed.
-         * @type {*}
-         */
-        var currentPageNumber = ko.observable(1);
+        var MAX_DISPLAYED_PAGE_COUNT = 7;
 
         /**
-         * A list of images corresponding to the current page.
-         * @type {*}
-         */
-        var currentPageImages = ko.computed(function() {
-            var totalThumbs = imageData().length;
-            var maxPageNumber = Math.ceil(totalThumbs/THUMBS_PER_PAGE);
-
-            if(totalThumbs == 0)
-                return [];
-            if (currentPageNumber() < 1 || currentPageNumber() > maxPageNumber)
-                throw {
-                    name: 'ValueError',
-                    message: 'The current page number is not within acceptable bounds'
-                }
-
-            var startingIndex = (currentPageNumber()-1) * THUMBS_PER_PAGE;
-            var endingIndex = startingIndex + THUMBS_PER_PAGE;
-            if (endingIndex > totalThumbs)
-                endingIndex = undefined;
-
-            return imageData.slice(startingIndex, endingIndex);
-        });
-
-        /**
-         * The entirety of the page names regardless of whether or not they can actually be seen by the user.
-         * @type {*}
-         */
-        var pageNames = ko.observableArray([]);
-        /**
-         * Generates methods for each of the pagination titles.
-         * @param pageNumber The page number of the title.
+         * Generic method for each of the pagination titles.
+         * @param pageNumber The page model instance
          * @return {Function}
          */
-        var pageActionGenerator = function(pageNumber) {
-            return function() {
-                var oldPageNumber = currentPageNumber();
-                pageNames()[oldPageNumber-1].isActive(false);
-                currentPageNumber(pageNumber);
-                pageNames()[pageNumber-1].isActive(true);
-            };
-        }
+        var genericPageAction = function(page) {
+            currentPage().isActive(false);
+            page.isActive(true);
+            currentPage(page);
+        };
         /**
          * Calculates which pages should be visible in the pagination bar such that the current page is always
          * centred.
          * @type {*}
          */
-        var visiblePageNames = ko.computed(function() {
-            var numberOfPages = Math.ceil(imageData().length/THUMBS_PER_PAGE);
+        var visiblePages = ko.computed(function() {
+            var numberOfPages = imageData().length;
             if (numberOfPages == 0)
                 return [];
 
-            var leftPadding = Math.floor((MAX_PAGE_TITLES-1)/2);
+            var leftPadding = Math.floor((MAX_DISPLAYED_PAGE_COUNT-1)/2);
             var leftPageIndex, rightPageIndex;
             var leftElipsesVisibility=false, rightElipsesVisibility=false;
 
-            leftPageIndex = currentPageNumber() - leftPadding - 1;
+            leftPageIndex = imageData.indexOf(currentPage()) - leftPadding;
             if (leftPageIndex < 0) {
                 leftPageIndex = 0;
             } else {
                 leftElipsesVisibility = true;
             }
-            rightPageIndex = leftPageIndex + MAX_PAGE_TITLES - 1;
+            rightPageIndex = leftPageIndex + MAX_DISPLAYED_PAGE_COUNT - 1;
             if (rightPageIndex >= numberOfPages){
                 rightPageIndex = numberOfPages-1;
-                leftPageIndex = rightPageIndex - MAX_PAGE_TITLES + 1;
+                leftPageIndex = rightPageIndex - MAX_DISPLAYED_PAGE_COUNT + 1;
             } else {
                 rightElipsesVisibility = true;
             }
-            var _visiblePageNames = pageNames.slice(leftPageIndex, rightPageIndex+1);
+            var _visiblePages = imageData.slice(leftPageIndex, rightPageIndex+1);
             if (leftElipsesVisibility)
-                _visiblePageNames.unshift(pageName('...', function() {}, false, false));
+                _visiblePages.unshift(ELLIPSES_PAGE);
             if (rightElipsesVisibility)
-                _visiblePageNames.push(pageName('...', function() {}, false, false));
-            return _visiblePageNames;
+                _visiblePages.push(ELLIPSES_PAGE);
+            return _visiblePages;
         });
 
         /**
-         * When the image data changes, the page names need to be updated.
+         * When the image urls variable changes, the image data needs to be refilled.
          */
-        imageData.subscribe(function() {
-            var totalThumbs = imageData().length;
+        imageUrls.subscribe(function() {
+            var totalThumbs = imageUrls().length;
             var maxPageNumber = Math.ceil(totalThumbs/THUMBS_PER_PAGE);
+            var currentImageUrlIdx = 0;
+            imageData([]);
 
             for (pageNumber = 1; pageNumber <= maxPageNumber; pageNumber++) {
-                pageNames.push(pageName(pageNumber, pageActionGenerator(pageNumber), false, true));
+                var pagePhotos = [];
+                for(var photoIdx = 0; photoIdx < THUMBS_PER_PAGE; photoIdx++) {
+                    if (currentImageUrlIdx < totalThumbs) {
+                        var currentImageUrls = imageUrls()[currentImageUrlIdx];
+                        pagePhotos.push(Models.photo(currentImageUrls['thumbnail'], currentImageUrls['original']));
+                        currentImageUrlIdx++;
+                    } else {
+                        break;
+                    }
+                }
+                imageData.push(Models.page(pagePhotos, pageNumber, genericPageAction, false, true));
             }
-            pageNames()[currentPageNumber()-1].isActive(true);
+            if (imageData().length) {
+                genericPageAction(imageData()[0]);
+                currentImage(imageData()[0].photos[0]);
+            } else
+                currentPage(EMPTY_PAGE);
         });
 
         /**
@@ -117,7 +96,7 @@ define(['ko', 'uberlearner/js/utils/dom'], function(ko, domUtils) {
             var urlParams = domUtils.urlParams();
             var funcNum = urlParams.CKEditorFuncNum;
             try {
-                window.opener.CKEDITOR.tools.callFunction(funcNum, currentImage().original);
+                window.opener.CKEDITOR.tools.callFunction(funcNum, currentImage().originalUrl);
             } catch (e) {
                 alert('Image could not be applied to the editor.');
             }
@@ -126,12 +105,12 @@ define(['ko', 'uberlearner/js/utils/dom'], function(ko, domUtils) {
 
         // PUBLIC SECTION
         return {
+            imageUrls: imageUrls,
             imageData: imageData,
-            currentPageImages: currentPageImages,
             currentImage: currentImage,
-            currentPageNumber: currentPageNumber,
             selectImage: selectImage,
-            visiblePageNames: visiblePageNames
+            visiblePages: visiblePages,
+            currentPage: currentPage
         };
     };
     return viewModelFactory;
