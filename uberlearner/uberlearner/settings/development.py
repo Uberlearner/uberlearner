d1cc5f59efd015cc161e9329ce09366cd6db6cfe
Abhin Chhabra <chhabra.abhin@gmail.com>
2012-12-29 15:55:30 -0700

    Moved the logging settings from development to deployment.

--- uberlearner/uberlearner/settings/development.py
+++ uberlearner/uberlearner/settings/development.py
@@ -10,33 +10,3 @@ DATABASES = {
         'NAME': os.path.join(PROJECT_ROOT, 'uberlearner.db'),
     }
 }
-
-LOGGING_ROOT = os.path.join(PROJECT_ROOT, '..', 'log')
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggirs': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s',
-        },
-    },
-    'handlers': {
-        'errors': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'formatter': 'verbose',
-            'filename': os.path.join(LOGGING_ROOT, 'errors.log'),
-        },
-    },
-    'loggers': {
-        'errors': {
-            'handlers': ['errors'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
