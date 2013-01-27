51aff9529bc100eb78b91b2c05f29fa0d8640483
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-01-26 17:59:30 -0700

    removed a return statement from the static file storage

--- uberlearner/main/staticfilestorage.py
+++ uberlearner/main/staticfilestorage.py
@@ -4,4 +4,4 @@ from storages.backends.s3boto import S3BotoStorage
 class S3StaticFileStorage(S3BotoStorage):
     def __init__(self, *args, **kwargs):
         kwargs['bucket'] = getattr(settings, 'STATIC_FILE_BUCKET', getattr(settings, 'AWS_STORAGE_BUCKET_NAME'))
-        return super(S3StaticFileStorage, self).__init__(*args, **kwargs)
+        super(S3StaticFileStorage, self).__init__(*args, **kwargs)
