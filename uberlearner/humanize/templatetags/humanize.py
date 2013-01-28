6d6c96cb362de8d486b8d174f63987b70cd4580c
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-01-28 16:54:26 -0700

    1) Added a link to the user's profile on the course detail page. 2) Added the estimated effort for the entire course.

--- uberlearner/humanize/templatetags/humanize.py
+++ uberlearner/humanize/templatetags/humanize.py
@@ -7,7 +7,7 @@ MINUTES_IN_HOUR = 60
 # This is the sub-hour resolution that the user will be exposed to. For example, if this value is 4, then the
 # resolution will be MINUTES_IN_HOUR/4 = 15 minutes. This means that 2 hours and 16 minutes will be rounded to
 # 2 hours and 15 minutes.
-INTERVALS_IN_HOUR = 4
+INTERVALS_IN_HOUR = 6
 SUB_HOUR_INTERVAL_SIZE = MINUTES_IN_HOUR / INTERVALS_IN_HOUR
 
 @register.filter(is_safe=False)
@@ -21,7 +21,7 @@ def humanize_minutes(value):
     extra_time = remaining_minutes % SUB_HOUR_INTERVAL_SIZE
     if extra_time > SUB_HOUR_INTERVAL_SIZE/2:
         sub_hour_intervals_remaining += 1
-        if sub_hour_intervals_remaining == 4:
+        if sub_hour_intervals_remaining == INTERVALS_IN_HOUR:
             hours += 1
             sub_hour_intervals_remaining = 0
     minutes = sub_hour_intervals_remaining * SUB_HOUR_INTERVAL_SIZE
